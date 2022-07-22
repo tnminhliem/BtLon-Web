5f776cfae5c66cd57bf06e146b744f0e2ab0dc32
minhliem03 <93208245+tnminhliem@users.noreply.github.com>
2022-07-22 20:50:36 +0700

    upload

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+var date = new Date();
+var week = weekdays[date.getUTCDay() - 1];
+var day = date.getUTCDate();
+var month = date.getUTCMonth() + 1;
+var year = date.getUTCFullYear();
+
+document.getElementById('time').innerHTML = week + ", " + day + "/" + month + "/" + year;
+
 var header = document.querySelector('header');
 var mobileMenu = document.querySelector('.list-icon');
-var nav = document.getElementById('nav-bar');
+var nav = document.getElementById('nav');
 var list = document.querySelector(".list-icon i");
-var x = document.querySelector(".closed-icon i");
 
 mobileMenu.onclick = function () {
     var isOpen = header.clientHeight === 50;
+
     if (isOpen) {
         header.style.height = '350px';
         nav.style.display = "block";
-    } else {
+    } 
+    else {
         header.style.height = null;
         nav.style.display = 'none';
     }
